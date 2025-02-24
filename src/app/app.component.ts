abd05ff122b21778462e25b82ae21dc35d6368f7
DonLavash <200119836+DonLavash@users.noreply.github.com>
2025-02-24 23:46:14 +0100

    render animated title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TitleAnimationComponent } from "./title-animation/title-animation.component";
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [TitleAnimationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
